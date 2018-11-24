5ae5104e8cc01838ce0f8eb9982f199463d37ca4
Lichtkind <lichtkind@cpan.org>
2018-11-24 21:15:26 +0100

    remove annoying header from markdown readme

--- update_readme.sh
+++ update_readme.sh
@@ -2,7 +2,7 @@
 
 echo "Generating README.md"
 
-echo "# Perl6-Math-Matrix\n\n[![Build Status](https://travis-ci.org/pierre-vigier/Perl6-Math-Matrix.svg?branch=master)](https://travis-ci.org/pierre-vigier/Perl6-Math-Matrix)" >README.md
+echo "[![Build Status](https://travis-ci.org/pierre-vigier/Perl6-Math-Matrix.svg?branch=master)](https://travis-ci.org/pierre-vigier/Perl6-Math-Matrix)" >README.md
 
 echo "[![Build status](https://ci.appveyor.com/api/projects/status/github/pierre-vigier/Perl6-Math-Matrix?svg=true)](https://ci.appveyor.com/project/pierre-vigier/Perl6-Math-Matrix/branch/master)\n" >>README.md
 
